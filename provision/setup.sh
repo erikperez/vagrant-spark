99e3c10cd203af11b043cc6fa57fdf2477ba4e4d
Erik Perez <erik.perez@comperiosearch.com>
2016-02-18 12:51:21 +0100

    Replaced spark-1.6.0 source with spark-1.6.0 prebuilt for hadoop 2.6. Added variable $SPARK_HOME to /usr/local/spark-1.6.0-bin-hadoop2.6

--- provision/setup.sh
+++ provision/setup.sh
@@ -16,7 +16,7 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-se
 apt-get install oracle-java8-installer -y -f
 
 #SCALA
-echo 'Downloading scala 2.11.7'
+echo 'Downloading Scala 2.11.7'
 wget http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.tgz
 mkdir /usr/local/src/scala
 tar xvf scala-2.11.7.tgz -C /usr/local/src/scala/
@@ -26,13 +26,11 @@ echo 'Setting environment variable SCALA_HOME and adding it to PATH'
 echo '#VAGRANT SETUP' >> .bashrc
 echo 'export SCALA_HOME=/usr/local/src/scala/scala-2.11.7' >> .bashrc
 echo 'export PATH=$PATH:$SCALA_HOME/bin' >> .bashrc
-
 #Reload bash
 . .bashrc
 
-echo 'Downloading spark 1.6.0'
-wget http://www.eu.apache.org/dist/spark/spark-1.6.0/spark-1.6.0.tgz
-tar xvf spark-1.6.0.tgz
-cd spark-1.6.0
-echo 'Building spark'
-build/sbt assembly
+echo 'Installing spark-1.6.0 pre-built for hadoop 2.6'
+wget http://www.eu.apache.org/dist/spark/spark-1.6.0/spark-1.6.0-bin-hadoop2.6.tgz
+tar xvf spark-1.6.0-bin-hadoop2.6.tgz -C /usr/local/
+echo 'export SPARK_HOME=/usr/local/spark-1.6.0-bin-hadoop2.6' >> .bashrc
+. .bashrc
